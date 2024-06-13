4addf01604bc611763865a2c44363593ec1b1d14
choise28 <choise28@gmail.com>
2024-06-13 19:47:16 +0900

    update

--- 3-7rotation2.py
+++ 3-7rotation2.py
@@ -6,6 +6,7 @@ rows,cols = img.shape[0:2]
 #-회전을 위한 변환 행렬 구하기
 # 회전축:중앙, 각도:45, 배율:0.5
 m45 = cv2.getRotationMatrix2D((cols/2,rows/2),45,0.5)
+
 # 회전축:중앙, 각도:90, 배율:1.5
 m90 = cv2.getRotationMatrix2D((cols/2,rows/2),90,1.5)
 
